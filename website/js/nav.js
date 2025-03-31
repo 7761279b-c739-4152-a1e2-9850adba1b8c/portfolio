e1440e3e93813a0643642336057a8b1eef653edb
7761279b-c739-4152-a1e2-9850adba1b8c <203860514+7761279b-c739-4152-a1e2-9850adba1b8c@users.noreply.github.com>
2025-03-31 11:56:47 +0100

    nav popout on main page

---
+++ website/js/nav.js
@@ -0,0 +1,28 @@
+let isopen = false;
+
+function open() {
+    for (let element of document.getElementsByTagName('nav')) {
+        element.className = 'visible';
+    }
+    isopen = true;
+}
+function close() {
+    if (isopen) {
+        for (let element of document.getElementsByTagName('nav')) {
+            element.className = '';
+        }
+        isopen = false;
+    }
+}
+
+document.getElementById('nav-burger').addEventListener('click', open);
+
+document.getElementsByClassName('main')[0].addEventListener('click', close);
+
+// the nav will close itself if the window is made larger and then shrunk
+// rather than remembering that it was open last time thew window was small
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 576) {
+        close();
+    }
+})
