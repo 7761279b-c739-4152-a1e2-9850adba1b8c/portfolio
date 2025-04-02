cf60eb865d72d081575911f0e428a53485be7060
7761279b-c739-4152-a1e2-9850adba1b8c <203860514+7761279b-c739-4152-a1e2-9850adba1b8c@users.noreply.github.com>
2025-04-02 09:54:50 +0100

    error dynamic changing

--- website/js/form.js
+++ website/js/form.js
@@ -1,42 +1,50 @@
 
 const email_regex = new RegExp(String.raw`^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$`)
 
+// invalid fields on last form check
+let invalids;
+let visible = false;
+
 function invalidFirstName() {
     const element = document.getElementById('first-name');
+    console.log(element.value);
     if (element.value != '') {
         element.className = 'valid'
         element.setCustomValidity("");
-        return 0;
+        invalids ^= (invalids & 0b1);
+    } else {
+        element.className = 'invalid'
+        element.setCustomValidity("First name is required");
+        invalids |= 0b1;
     }
-    element.className = 'invalid'
-    element.setCustomValidity("First name is required");
-    return 0b1;
 }
 function invalidLastName() {
     const element = document.getElementById('last-name');
     if (element.value != '') {
         element.className = 'valid'
         element.setCustomValidity("");
-        return 0;
+        invalids ^= (invalids &= 0b10);
+    } else {
+        element.className = 'invalid'
+        element.setCustomValidity("Last name is required");
+        invalids |= 0b10;
     }
-    element.className = 'invalid'
-    element.setCustomValidity("Last name is required");
-    return 0b10;
 }
 function invalidEmail() {
     const element = document.getElementById('email');
     if (email_regex.test(element.value)) {
         element.className = 'valid'
         element.setCustomValidity("");
-        return 0;
-    }
-    element.className = 'invalid'
-    if (element.value == '') {
-        element.setCustomValidity("Email address is required");
-        return 0b100;
+        invalids ^= (invalids & 0b1100);
     } else {
-        element.setCustomValidity("Invalid email address");
-        return 0b1000;
+        element.className = 'invalid'
+        if (element.value == '') {
+            element.setCustomValidity("Email address is required");
+            invalids |= 0b100;
+        } else {
+            element.setCustomValidity("Invalid email address");
+            invalids |= 0b1000;
+        }
     }
 }
 function invalidSubject() {
@@ -44,26 +52,32 @@ function invalidSubject() {
     if (element.value != '') {
         element.className = 'valid'
         element.setCustomValidity("");
-        return 0;
+        invalids ^= (invalids & 0b10000);
+    } else {
+        element.className = 'invalid'
+        element.setCustomValidity("Subject must not be empty");
+        invalids |= 0b10000;
     }
-    element.className = 'invalid'
-    element.setCustomValidity("Subject must not be empty");
-    return 0b10000;
 }
 function invalidMessage() {
     const element = document.getElementById('message');
     if (element.value != '') {
         element.className = 'valid'
         element.setCustomValidity("");
-        return 0;
+        invalids ^= (invalids & 0b100000);
+    } else {
+        element.className = 'invalid'
+        element.setCustomValidity("Message must not be empty");
+        invalids |= 0b100000;
     }
-    element.className = 'invalid'
-    element.setCustomValidity("Message must not be empty");
-    return 0b100000;
 }
 
-function showErrorMessage(invalids) {
+function updateErrorMessage() {
     hideErrorMessage();
+    if (invalids == 0) {
+        return;
+    }
+    visible = true;
     const element = document.createElement('div');
     element.id = "formError"
     let content = "<p>Unable to submit form:</p>";
@@ -85,7 +99,6 @@ function showErrorMessage(invalids) {
     if (invalids & 0b100000) {
         content += "<p>Message is required</p>";
     }
-    console.log(content);
     element.innerHTML = `<div>${content}</div>`;
     const close = document.createElement('a');
     close.addEventListener('click', hideErrorMessage);
@@ -97,28 +110,48 @@ function hideErrorMessage() {
     if (element) {
         element.remove();
     }
+    visible = false;
 }
 
 document.getElementById('submit').addEventListener('click', () => {
-    let invalids;
-    if ((invalids = (
-        invalidFirstName() |
-        invalidLastName() |
-        invalidEmail() |
-        invalidSubject() |
-        invalidMessage()
-    )) != 0) {
-        showErrorMessage(invalids);
-    } else {
-        hideErrorMessage();
-    }
+    invalidFirstName();
+    invalidLastName();
+    invalidEmail();
+    invalidSubject();
+    invalidMessage();
+    updateErrorMessage();
 })
 
-document.getElementById('first-name').addEventListener('focusout', invalidFirstName);
-document.getElementById('last-name').addEventListener('focusout', invalidLastName);
-document.getElementById('email').addEventListener('focusout', invalidEmail);
-document.getElementById('subject').addEventListener('focusout', invalidSubject);
-document.getElementById('message').addEventListener('focusout', invalidMessage);
+document.getElementById('first-name').addEventListener('focusout', () => {
+    invalidFirstName();
+    if (visible) {
+        updateErrorMessage();
+    }
+});
+document.getElementById('last-name').addEventListener('focusout', () => {
+    invalidLastName();
+    if (visible) {
+        updateErrorMessage();
+    }
+});
+document.getElementById('email').addEventListener('focusout', () => {
+    invalidEmail();
+    if (visible) {
+        updateErrorMessage();
+    }
+});
+document.getElementById('subject').addEventListener('focusout', () => {
+    invalidSubject();
+    if (visible) {
+        updateErrorMessage();
+    }
+});
+document.getElementById('message').addEventListener('focusout', () => {
+    invalidMessage();
+    if (visible) {
+        updateErrorMessage();
+    }
+});
 
 document.getElementsByTagName('form')[0].addEventListener('submit', (event) =>  {
     event.preventDefault();
