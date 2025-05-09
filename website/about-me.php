5b6e88f42a02bf69bb73c3828ed245f751fa3442
7761279b-c739-4152-a1e2-9850adba1b8c <203860514+7761279b-c739-4152-a1e2-9850adba1b8c@users.noreply.github.com>
2025-05-09 10:08:18 +0100

    change placeholder about me

--- website/about-me.php
+++ website/about-me.php
@@ -9,8 +9,20 @@ require 'parts/header.php';
     </a>
     <div class="about-me container">
         <h1>About me</h1>
-        <h3>Placeholder text</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p></p>
+        <p>Through the Netmatters Scion Scheme I have been taught the following:</p>
+        <ul>
+            <li><p>HTML</p></li>
+            <li><p>CSS and SASS</p></li>
+            <li><p>JavaScript</p></li>
+            <li><p>SQL</p></li>
+            <li><p>PHP</p></li>
+            <!-- <li><p>C#</p></li> -->
+
+            <li><p>git</p></li>
+        </ul>
+        <p>I have also previously self-taught on some programming languages such as Python.</p>
+        <p>I feel I have a strong understanding of basic programming concepts which has helped me when learning new languages</p>
     </div>
 </div>
 <?php require 'parts/footer.php'; ?>
